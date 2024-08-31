a967e509694e522db270d02ceca7779105fa75a5
Razu Ahamed <en.razu.ahamed@gmail.com>
2024-08-31 16:46:11 +0600

    read from excel and write to database method add

--- src/main/java/com/razuahamed/springboot/project_management/service/ExcelService.java
+++ src/main/java/com/razuahamed/springboot/project_management/service/ExcelService.java
@@ -4,9 +4,12 @@ import com.razuahamed.springboot.project_management.model.Member;
 import com.razuahamed.springboot.project_management.repository.TaskRepository;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -34,7 +37,26 @@ public class ExcelService {
             row.createCell(5).setCellValue(member.getName());
             row.createCell(6).setCellValue("Confidential");
         }
-
         return workbook;
+    }
+
+    public void updateMemberFromExcel(MultipartFile memberList) {
+        /*
+         * Id , active, age, Email, Image_path, name, Password
+         * */
+        XSSFWorkbook workbook = null;
+        try {
+            workbook = new XSSFWorkbook(memberList.getInputStream());
+            XSSFSheet members = workbook.getSheetAt(0);
+            int totalCount = 0, totalUpdatedResult = 0;
+            for (Row member : members) {
+                if (member.getRowNum() == 0 || member.getCell(0) == null) {
+                    continue; // Skip header or incomplete first row
+                }
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
